fa6dcee3d21d072f3d1b155bad77c197b3520bcb
KLan Stovall <kland13@gmail.com>
2021-09-30 15:13:11 -0700

    fleshed out catalog page

--- js/app.js
+++ js/app.js
@@ -3,20 +3,33 @@
 // Cart constructor.
 const Cart = function(items) {
   // this.items is an array of CartItem instances.
+
   this.items = items;
 };
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  let item = new CartItem (product, quantity);
+  this.items.push(item);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  let cartArray = this.items;
+  let modifiedCartArray = JSON.stringify(cartArray);
+  console.log(cartArray);
+  console.log(modifiedCartArray);
+
+  localStorage.setItem('cart', modifiedCartArray);
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  let returnedCartArray = localStorage.getItem('cart');
+  let cartPageArray = JSON.parse(returnedCartArray);
+
+  
 };
 
 const CartItem = function(product, quantity) {
--- js/catalog.js
+++ js/catalog.js
@@ -8,11 +8,16 @@ const cart = new Cart([]);
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
-
+  
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
-  for (let i in Product.allProducts) {
-
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    let createOption = document.createElement('option');
+    console.log('hello');
+    let product = Product.allProducts[i].name;
+    createOption.innerText = product;
+    createOption.setAttribute('value', product);
+    selectElement.appendChild(createOption);
   }
 
 }
@@ -22,30 +27,45 @@ function populateForm() {
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
 
+  event.preventDefault();
   // TODO: Prevent the page from reloading
+  const {items, quantity} = event.target;
+  console.log(items.value);
 
   // Do all the things ...
-  addSelectedItemToCart();
+  addSelectedItemToCart(items.value, quantity.value);
   cart.saveToLocalStorage();
-  updateCounter();
-  updateCartPreview();
+  updateCounter(items.value, quantity.value);
+  updateCartPreview(items.value, quantity.value);
 
 }
 
 // TODO: Add the selected item and quantity to the cart
-function addSelectedItemToCart() {
+function addSelectedItemToCart(items, quantity) {
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
+  cart.addItem(items, quantity);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  let itemCount = document.getElementById('itemCount');
+  itemCount.innerText = cart.items.length + 1;
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
-function updateCartPreview() {
+function updateCartPreview(items, quantity) {
   // TODO: Get the item and quantity from the form
   // TODO: Add a new element to the cartContents div with that information
+  let parentEl = document.getElementById('cartContents');
+  let itemTitle = document.createElement('h3');
+  console.log(items);
+  itemTitle.innerText = items;
+  let quantityTitle = document.createElement('p');
+  quantityTitle.innerText = quantity;
+  parentEl.appendChild(itemTitle);
+  parentEl.appendChild(quantityTitle);
 }
 
 // Set up the "submit" event listener on the form.
